506b02b8380c1aa4966c0d6ce769103f45f69d2f
Akil Mullassery Prashant <94084424+akilmp@users.noreply.github.com>
2025-07-25 21:05:03 +1000

    Add pipeline scaffolding

---
+++ dbt/models/dim_route.sql
@@ -0,0 +1,2 @@
+-- example model for demo purposes
+select 1 as route_id, 'A' as route_short_name, 'Route A' as route_long_name
---
+++ dbt/models/fact_trip_punctuality.sql
@@ -0,0 +1,5 @@
+-- example incremental model
+select 1 as trip_id,
+       1 as route_id,
+       current_date as service_date,
+       0.0 as delay_min
